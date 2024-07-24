3283b8ed0d645b1fa9ff6eb3b7b01cc1c2b38442
sebastianmonten <sebastianmonten.tech@gmail.com>
2024-07-24 14:02:56 +0200

    feat: implement a basic connection between a pre and post neuron

--- src/main.c
+++ src/main.c
@@ -10,6 +10,7 @@ typedef struct {
 
 bool LIF_update(LIFNeuron *self, double input, double dt) {
     double dmp = (-(self->mp - self->rp) + input) * (dt / self->tau);
+
     self->mp += dmp;
 
     if (self->mp >= self->ath) {
@@ -24,7 +25,8 @@ bool LIF_update(LIFNeuron *self, double input, double dt) {
 
 int main() {
 
-    LIFNeuron n1 = {6.0, 0.0, 0.06, 0.0};
+    LIFNeuron n1 = {6.0, 0.01, 0.06, 0.01};
+    LIFNeuron n2 = {6.0, 0.01, 0.06, 0.01};
 
     FILE *fptr;
 
@@ -32,21 +34,27 @@ int main() {
     fptr = fopen("bin/tmp_output.csv", "w+");
 
     // Write some text to the file
-    fprintf(fptr, "Time,Membrane Potential\n");
+    fprintf(fptr, "Time,MemPot 1, MemPot 2\n");
     
     double dt = 0.1;
     double t = 0.0;
-    double inputs[100];
-    inputs[0] = 1.8;
-    inputs[12] = 1.8;
-    inputs[30] = 1.8;
-    inputs[50] = 1.8;
-    inputs[70] = 1.8;
-    fprintf(fptr, "%f,%f\n", t, n1.mp);
+    double inputs[100] = {0};
+
+    inputs[0] = 2;
+    inputs[30] = 2;
+    inputs[50] = 2;
+    inputs[80] = 2;
+
+    fprintf(fptr, "%f,%f,%f\n", t, n1.mp, n2.mp);
     for (int i = 0; i < 100; i++) {
         t += dt;
-        bool spike = LIF_update(&n1, inputs[i], dt);
-        fprintf(fptr, "%f,%f\n", t, n1.mp);
+        bool spike1 = LIF_update(&n1, inputs[i], dt);
+        if (spike1) {
+            bool spike2 = LIF_update(&n2, 2.0, dt);
+        } else {
+            LIF_update(&n2, 0.0, dt);
+        }
+        fprintf(fptr, "%f,%f,%f\n", t, n1.mp, n2.mp);
     }
 
     // Close the file
--- src/plot.py
+++ src/plot.py
@@ -1,13 +1,29 @@
-import pandas as pd
+# This script is created with ChatGPT
+
+import numpy as np
 import matplotlib.pyplot as plt
 
-# Read the CSV file
-df = pd.read_csv('bin/tmp_output.csv')
+# Read the CSV file into NumPy arrays
+data = np.genfromtxt('bin/tmp_output.csv', delimiter=',', skip_header=1)
 
-# Plot the data
-plt.plot(df['Time'], df['Membrane Potential'])
-plt.xlabel('Time (s)')
-plt.ylabel('Membrane Potential (V)')
-plt.title('Membrane Potential over Time')
-plt.grid(True)
+# Separate the columns
+time = data[:, 0]
+mem_pot_1 = data[:, 1]
+mem_pot_2 = data[:, 2]
+
+# Create a figure and two subplots
+fig, (ax1, ax2) = plt.subplots(2, 1, sharex=True)
+
+# Plot MemPot 1
+ax1.plot(time, mem_pot_1, label='MemPot 1')
+ax1.set_ylabel('MemPot 1')
+ax1.legend()
+
+# Plot MemPot 2
+ax2.plot(time, mem_pot_2, label='MemPot 2', color='orange')
+ax2.set_ylabel('MemPot 2')
+ax2.set_xlabel('Time')
+ax2.legend()
+
+# Show the plot
 plt.show()
